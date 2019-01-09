3ffffaaae55bf06a928173999f1cbabc034f3f8f
212991 <44865991+212991@users.noreply.github.com>
2019-01-09 14:33:10 +0100

    update

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ const gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync');
+var postcss = require('gulp-postcss');
+
+var autoprefixer = require('autoprefixer');
+var cssnano = require('gulp-sass');
 
 sass.compiler = require('node-sass');
 
@@ -19,9 +23,14 @@ gulp.task('serve', ['scss'], function() {
 
 // taak aanmaken
 gulp.task('scss', function(){
+var processors = [
+  autoprefixer,
+  cssnano
+];
 return gulp.src('./gulp/scss/**/*.scss')
 .pipe(sourcemaps.init())
 .pipe(sass().on('error', sass.logError))
+.pipe(postcss(processors))
 .pipe(sourcemaps.write('./map'))
 .pipe(gulp.dest('./dist'))
 .pipe(browserSync.stream());
